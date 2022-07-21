c1e9c742a560aa71eb8beadcc2294b9571f70aee
leonlin <l84328g@gmail.com>
2022-07-21 21:45:47 +0800

    note

--- model/build_model.py
+++ model/build_model.py
@@ -59,25 +59,14 @@ def get_model_names():
 
 
 def build_model(model_name):
-    # main()
+    # TODO: import clss only defined inside file
+    # TODO: To clarify module and sub-module
     """Build up model from provided model name"""
-    # import pkgutil
-    # it = pkgutil.iter_modules([dirname(__file__), join(dirname(__file__), 'pytorch')])
-    # it2 = pkgutil.walk_packages([dirname(__file__), join(dirname(__file__), 'pytorch')])
-    # for a in it2:
-    #     print(a)
-    # for finder, name, _ in it:
-    #     print(finder.path, name)
-    #     importlib.import_module('{}.{}'.format(finder.path, name))
-
-    # print(__file__)
-    # a = join(dirname(__file__), 'pytorch', "*.py")
     from pathlib import Path
     modules = glob.glob(join(dirname(__file__), 'pytorch', '**', "*.py"), recursive=True)
     modules = [ f for f in modules if isfile(f) and not f.endswith('__init__.py')]
-    # __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
-    mapping = {}
+    cls_mapping = {}
     for module_path in modules:
         relative_path = os.path.relpath(module_path, os.getcwd())
         dir_parts = Path(relative_path).parts
@@ -86,17 +75,11 @@ def build_model(model_name):
         classes = getmembers(module, isclass)
         for name, cls in classes:
             print(name, cls, cls.__bases__)
-            mapping[f'{module_imp_name}.{name}'] = cls
-    pprint(mapping)
+            if cls.__module__.startswith(module_imp_name):
+                cls_mapping[f'{module_imp_name}.{name}'] = cls
+    pprint(cls_mapping)
     print(3)
-    # model = importlib.import_module(f'model.{model_name}')
-    # u = model.unet_2d
-    # subclass = model.__subclasses__()
-    # unet = model.UNet_2d
-    # unet2 = unet(input_channels=1, num_class=4)
-    # print(model)
-    # return model
-
+    
 
 def build_seg_model(model_name, in_planes, n_class, pytorch_pretrained=True):
     segmnetation_model = select_model(model_name, in_planes, n_class, pytorch_pretrained)
